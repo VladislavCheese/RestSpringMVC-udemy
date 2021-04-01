a8ae0d9a17cd9b3324e62d078052f53a039ec9e0
VladislavS <89851502703l@gmail.com>
2021-04-01 12:23:22 +0300

    + initDB

--- src/main/resources/DB/initDB.sql
+++ src/main/resources/DB/initDB.sql
@@ -1,11 +1,11 @@
 drop table if exists people;
 CREATE TABLE people (
-                           id serial NOT NULL,
+                           people_id serial NOT NULL,
                            name varchar(15),
                            surname varchar(25),
                            department varchar(20),
                            salary int,
-                           PRIMARY KEY (id)
+                           PRIMARY KEY (people_id)
 ) ;
 
 INSERT INTO people(name, surname, department, salary)
